e18226e6506d0124dd8a9c78130d9e2433185ab7
Faras Momin <famomin@utexas.edu>
2017-01-19 15:00:57 -0600

    Replacement file for keys.js, no keys in this file

---
+++ keys.sample.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'secretConsKeyHere',
+  consumer_secret: 'consSecretHere',
+  access_token_key: 'secAccessKeyHere',
+  access_token_secret: 'accessSecretHere',
+}
